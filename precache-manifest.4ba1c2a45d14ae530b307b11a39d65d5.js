d83b249024a5739b9de51794f02edb902aa4109f
ovgamesdev <vova40227@gmail.com>
2021-11-21 17:45:50 +0600

    Update

---
+++ precache-manifest.4ba1c2a45d14ae530b307b11a39d65d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80dceb85a39f0b194a449a504d19205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a47b60da2a46a47caa06",
+    "url": "/static/css/main.80aadb03.chunk.css"
+  },
+  {
+    "revision": "8e778119158f2d2212d9",
+    "url": "/static/js/2.b25a0210.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.b25a0210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47b60da2a46a47caa06",
+    "url": "/static/js/main.b5bdc758.chunk.js"
+  },
+  {
+    "revision": "ea7cee67bfe696615ef9",
+    "url": "/static/js/runtime-main.4c6c3c1c.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
